cf517855817a89555d27130e4eb0fc7106e40310
Samin Zachariah <saminzachariah@seattleacademy.org>
2017-02-15 09:49:41 -0800

    fix config

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ var config = {
           , "hud"           // Display the artificial horizon, altimeter, compass, etc.
           , "battery"       // Display a simple battery widget in the header bar
           , "pilot"         // Pilot the drone with the keyboard
-          , "tracker"       // track pixels in webflight
-          , "test"
+          , "tracker"
+
         ],
 
         // Config for pilot plugin
